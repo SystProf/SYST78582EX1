007e3fde8610d23bef1714f96f3d0289b0313386
megha <megha.patel2@sheridancollege.ca>
2022-01-25 10:23:00 -0500

    first commit

---
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+/**
+ * A class that fills a magic hand of 7 cards with random Card Objects
+ * and then asks the user to pick a card and searches the array of cards
+ * for the match to the user's card. To be used as starting code in ICE 1
+ * @author dancye
+ * @modified by Megha Patel
+ */
+public class CardTrick {
+    
+    public static void main(String[] args)
+    {
+        Card[] magicHand = new Card[7];
+        
+        for (int i=0; i<magicHand.length; i++)
+        {
+            Card c = new Card();
+             }
+        
+         }
+    
+}
