926aa7b06a9643e5fccb9c123b98552907a34740
farishta <farishta@shernet.sheridancollege.ca>
2022-01-27 16:16:44 -0500

    my first commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,12 +5,18 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
- * @modified by Megha Patel
+ * @author saniya
+ * student Id = 991631555
+ * date: 27 january 2022
+ * @modified by Saniya Farishta
  * 
  */
 public class CardTrick {
@@ -18,12 +24,46 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-             }
-        
-         }
-    
+       Random ran = new Random();
+       for(int i=0;i<magicHand.length;i++)
+       {
+           Card c = new Card();
+          
+           c.setValue(ran.nextInt(13)+1); 
+           c.setSuit(Card.SUITS[ran.nextInt(4)]); 
+           magicHand[i] = c; 
+       }
+      
+       
+       Card userCard = new Card();
+       int value;
+       String suit;
+       boolean found = false;
+      
+       Scanner scan = new Scanner(System.in);
+       
+       System.out.print("Enter the value of the card(1-13): ");
+       value = scan.nextInt();
+       System.out.print("Enter the suit of the card"+Arrays.toString(Card.SUITS)+": ");
+       suit = scan.next();
+      
+       
+       userCard.setValue(value);
+       userCard.setSuit(suit);
+      
+       
+        for (Card magicHand1 : magicHand) {
+            if (magicHand1.getValue() == userCard.getValue() && magicHand1.getSuit().equalsIgnoreCase(userCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+      
+       
+       if(found)
+           System.out.println("User card found in magicHand");
+       else
+           System.out.println("User card not found in magicHand");
+   }
+
 }
