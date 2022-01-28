912cb52491b629d5baa29be9db597ba36124ffbd
Sheridan Archer <archsher@sheridancollege.ca>
2022-01-28 00:05:16 -0500

    I'm done!

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -7,13 +7,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
  * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * a value (note that Ace = 1, Jack =11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye
- * @modified by Megha Patel
+ * @author Sheridan Archer
+ * @modified by Sheridan Archer
  * 
  */
 public class Card {
@@ -50,6 +50,4 @@ public class Card {
         this.value = value;
     }
    
-   
-    
 }
