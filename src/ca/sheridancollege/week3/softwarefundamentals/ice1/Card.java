6848782631b72d36b49fb77cedbb530b35eb24cb
amaan <amaan@LAPTOP-J2HA453K>
2022-01-29 12:18:12 -0500

    edited-amaan-sheikh

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -5,51 +5,78 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Random;
+import sun.awt.SunHints;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * a value (note that Ace = 1, Jack =11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye
- * @modified by Megha Patel
+ * @author amaan
+ * @modified by AmaanSheikh
  * 
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+    String randElement;
+    int valueNum;
+    
+   //public String suit; //clubs, spades, diamonds, hearts
+   //public int value;//1-13
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
+/*
+   Random random = new Random();
+   String randomString = SUITS[random.nextInt(SUITS.length)];
+*/
+   
+   //int randIndex = ThreadLocalRandom.current().nextInt(SUITS.length);
+   //String randElement = SUITS[randIndex];
+   //return randElement;
+   
+   public void Card(String randElement,int valueNum) {
+       this.randElement = "a";
+       this.valueNum = 2;
+    }
+    
+    
+   /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String newRandElement) {
+        this.randElement = newRandElement;
+    }
+   /**
      * @return the suit
      */
     public String getSuit() {
-        return suit;
+        return this.randElement;
     }
 
     /**
-     * @param suit the suit to set
+     * @param value the value to set
      */
-    public void setSuit(String suit) {
-        this.suit = suit;
+    public void setValue(int newValueNum) {
+        this.valueNum = newValueNum;
     }
 
     /**
      * @return the value
      */
     public int getValue() {
-        return value;
+        //value = (int)Math.random()*13;
+        return this.valueNum;
     }
 
-    /**
-     * @param value the value to set
-     */
-    public void setValue(int value) {
-        this.value = value;
-    }
+
    
-   
+   @Override
+ public String toString(){
+     return "number: " + this.valueNum + "Suit: " + this.randElement;
+ }
     
 }
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,25 +5,35 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import static ca.sheridancollege.week3.softwarefundamentals.ice1.Card.SUITS;
+//import com.sun.org.apache.bcel.internal.generic.InstructionConst;
+//import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+//import sun.awt.SunHints;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
- * @modified by Megha Patel
- * 
+ * @modified by AmaanSheikh
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
+
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-             }
+        for (int i=0; i<magicHand.length; i++){
+            int randIndex = ThreadLocalRandom.current().nextInt(SUITS.length);
+            String randElement = SUITS[randIndex];
+            int valueNum = ThreadLocalRandom.current().nextInt(1,13);
+            ///magicHand[i] = (this.randElement,valueNum);
+            
+        }
         
          }
+    
     
 }
