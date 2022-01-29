a4dcc393e772276148296a1af1f32a52323af15e
pate3053 <pate3052@sheridancollege.ca>
2022-01-28 22:26:30 -0500

    First Commit

---
+++ src/pickacard/Card.java
@@ -0,0 +1,49 @@
+package pickacard;
+
+/*
+* Name: Priyanshu patel
+* Student id: 991624476
+*/
+
+public class Card {
+
+   private String suit; //clubs, spades, diamonds, hearts
+   private int value;//1-13
+   
+   public Card(int value, String suit){
+       this.value = value;
+       this.suit = suit;
+   }
+   public Card(){}
+   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+   
+      
+}
---
+++ src/pickacard/CardTrick.java
@@ -0,0 +1,56 @@
+package pickacard;
+
+/*
+ * Name: Priyanshu Patel
+ * Student ID: 991624476
+*/
+import java.util.Scanner;
+
+public class CardTrick {
+    
+    public static void main(String[] args)
+    {
+        Card[] magicHand = new Card[7];
+        
+        for(int i=0; i<magicHand.length; i++)
+        {
+            magicHand[i] = new Card();
+            magicHand[i].setValue((int)(Math.random()*13)+1);
+            magicHand[i].setSuit(Card.SUITS[(int)(Math.random()*4)]);
+        }
+        for (Card magicHand1 : magicHand) {
+            System.out.printf("%-10d%s%n", magicHand1.getValue(), 
+                    magicHand1.getSuit());
+        }
+        Scanner input = new Scanner(System.in);
+        
+        Card c1 = new Card();
+        System.out.printf("%-50s", "Enter your desired card number");
+        c1.setValue(input.nextInt());
+        System.out.printf("%-50s", "Enter your desired suit");
+        c1.setSuit(input.next());
+        
+        //Hard coded luckyCard
+        Card luckyCard = new Card(1,"Spades");
+        
+        int control =0;
+        for (Card magicHand1 : magicHand) {
+            if ((c1.getSuit().equalsIgnoreCase(magicHand1.getSuit())) && 
+                    (c1.getValue() == magicHand1.getValue())){
+                
+                
+                System.out.printf("%s%d %s%s%n","Magic hand contains "
+                        + "your card(",c1.getValue(),c1.getSuit(),")");
+                control =1;
+                break;
+            } 
+        }
+        
+        
+        if(control==0){
+            System.out.printf("%s%d %s%s%n","Magic hand does not contain"
+                    + " your card(",c1.getValue(),c1.getSuit(),")");
+        }
+    }
+}
+    
